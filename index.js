159dae73ecaf8187f714f1cba2b9dc738c0f5fe0
Cullen Gandy <cullen.gandy@gmail.com>
2018-11-23 19:39:25 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,8 +21,7 @@ function sayHiToGrandma(string) {
   if (string.toUpperCase() === string) {
     return "YES INDEED!"
   }  
-  if (string === "I love you, Grandma") {
-    return "I love you,too."
-  } 
-  
+  if (string === "Ilove you,Grandma.") {
+    return "I love you, too."
+  }
 }
